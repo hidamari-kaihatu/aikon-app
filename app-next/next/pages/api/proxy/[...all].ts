9d7546a409d22e2da5ddaae9f0042fc2cb21a49a
Haruka Nakashima <haruchanchan11@gmail.com>
2022-09-29 12:37:52 +0900

    使用する環境変数の変更

--- app-next/next/pages/api/proxy/[...all].ts
+++ app-next/next/pages/api/proxy/[...all].ts
@@ -4,7 +4,8 @@ import httpProxyMiddleware from "next-http-proxy-middleware";
 export default (req: NextApiRequest, res: NextApiResponse) =>
   httpProxyMiddleware(req, res, {
     // target: `http://host.docker.internal:8080`,
-    target: process.env.PROXY_TARGET_URL,
+    // target: `${process.env.PROXY_TARGET_URL}`,
+    target: `${process.env.API}`,
     pathRewrite: [
       {
         patternStr: "^/api/proxy",
