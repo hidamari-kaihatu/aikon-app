f72178b103a41fea4bd97c23263929d4ec824603
HINAKO <artstudy.sun175@gmail.com>
2022-10-04 11:01:00 +0900

    teacherとparentのレイアウト（ヘッダーとフッター）のファイルのPR

---
+++ app-next/next/pages/parents/parent-footer.tsx
@@ -0,0 +1,10 @@
+import Today from '../../components/date';
+
+export default function Footer() {
+    return (
+        <>
+        <Today />
+        <p>☆❤△さん（ここにログインした人(子供)のnameが入る）</p>
+        </>
+    );
+}
