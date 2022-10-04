c5c694073ecafe91ccfd014e6c213797f147247b
HINAKO <artstudy.sun175@gmail.com>
2022-10-04 11:04:52 +0900

    teacherフォルダのファイルの修正

--- app-next/next/pages/teachers/teacher-kidsinfo.tsx
+++ app-next/next/pages/teachers/teacher-kidsinfo.tsx
@@ -1,1 +1,77 @@
-import type { NextPage } from 'next'
+import Layout from "./teacher-layout";
+import axios from "axios";
+import { Axios } from '../../lib/api';
+import React,{ useState } from "react";
+
+export async function getServerSideProps() {
+  const res = await axios.get(`${process.env.API}/dailyReportGet`, {
+  });
+  const data = await res.data;
+
+  return { 
+      props: {
+        data: data,
+      },
+  };
+}
+
+const StudentGet = ({data}:any) => {
+console.log(data)
+/* let n = 1;
+function counter(){
+  return n++;
+}; */
+
+function liftButton (id:any) {
+      // e.preventDefault();
+      const teacher = {id: id};
+      Axios.put(`api/proxy/stuStatustPut`, teacher)
+      .then((res) => {
+      })
+      .catch((error) => {
+      });
+      window.location.reload()
+    }
+
+return (
+<Layout>
+    <table className='list-table'>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Date</th>
+          <th>Student_ID</th>
+          <th>Attend</th>
+          <th>Temperature</th>
+          <th>topickup</th>
+          <th>timepickup</th>
+          <th>message</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item:any, i:number) => {
+              return (
+                  <>
+                  <tr  key={item}>
+                   <td>{item.Id}</td>
+                   <td>{item.Date}</td>
+                   <td>{item.Student_id}</td>
+                   <td>{item.Attend.toString()}</td>
+                   <td>{item.Temperature}</td>
+                   <td>{item.SomeoneToPickUp}</td>
+                   <td>{item.TimeToPickUp}</td>
+                   <td>{item.Message}</td>
+                   <td> <button onClick = {() => {liftButton(item.Id)}}>除籍</button></td>
+                  </tr>
+                  </>   
+              )
+      }
+    )}
+      </tbody>
+    </table>
+    </Layout>
+    )
+  }
+  
+  export default StudentGet
+
--- app-next/next/pages/teachers/teacher-sendMsg.tsx
+++ app-next/next/pages/teachers/teacher-sendMsg.tsx
@@ -1,1 +1,38 @@
-import type { NextPage } from 'next'
+import React, { useState } from "react";
+import StartButton from "../../components/startButton";
+import Output from "../../components/output";
+import Layout from "../teachers/teacher-layout";
+
+function App() {
+  const[fromPerson, setFromPerson]=useState("")
+  const[toPerson, setToPerson]=useState("")
+  const submit = () => {
+    window.location.reload()
+  }
+  
+  return (
+    <>
+    <Layout>
+      <h1>先生が保護者に音声でメッセージを送るページ</h1>
+      <div>
+      <label>誰から送る: </label>
+      <select value={fromPerson} onChange={(e) => setFromPerson(e.target.value)}>
+        <option value="A">先生の名前</option>
+        <option value={"高井日菜子"}>高井日菜子</option>
+      </select>
+      <br></br>
+      <label>誰へ送る: </label>
+      <select value={toPerson} onChange={(e) => setToPerson(e.target.value)}>
+        <option value="A">生徒の名前</option>
+        <option value={"HINAKO TAKAI"}>HINAKO TAKAI</option>
+      </select>
+      <StartButton />
+      <Output /> 
+      </div>
+      <button onClick={submit}>送信する</button> 
+      </Layout>
+    </>
+  );
+}
+  
+export default App;
