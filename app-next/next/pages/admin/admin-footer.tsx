822809d2f7ee0570418fec1c7eeb9bcf032d9927
Haruka Nakashima <haruchanchan11@gmail.com>
2022-10-05 11:22:02 +0900

    コードの微修正

--- app-next/next/pages/admin/admin-footer.tsx
+++ app-next/next/pages/admin/admin-footer.tsx
@@ -3,7 +3,9 @@ import Link from 'next/link';
 export default function Footer() {
     return (
         <>
+        <p>◯月◯日◯曜日</p>
         <p>管理者〇〇さんのページです</p>
+        <button>ログアウト</button>
         </>
     );
   }
