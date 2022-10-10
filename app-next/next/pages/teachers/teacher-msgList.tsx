64d99f12f09b173d886293d59aa0de516e232083
HINAKO <artstudy.sun175@gmail.com>
2022-10-10 17:01:36 +0900

    teacher-msgListのcss

--- app-next/next/pages/teachers/teacher-msgList.tsx
+++ app-next/next/pages/teachers/teacher-msgList.tsx
@@ -28,15 +28,19 @@ export default function mypage({data, staffs}:any) {
     return (
       <>
       <Layout>
-        <h2>保護者に送ったメッセージの一覧</h2>
+{/*         <h2>保護者に送ったメッセージの一覧</h2> */}
+        <div className='msgblueback'>
         {data.map((d) => (
+                  <div className='sendmsg'>
         <ul key={d.Id}>
             日付: {d.Datetime}<br/><br/>メッセージ: {d.Message}<br/><br/><hr/>
         </ul>
+              </div>
         ))}
-        {staffs[0].CenterName}
+{/*         {staffs[0].CenterName}
         <br></br>
-        {staffs[0].Name}
+        {staffs[0].Name} */}
+              </div>
         </Layout>
       </>
     );
